49f7b9bf438d800db49b069489f1489be43e9575
defolym3 <defolym3@gmail.com>
2024-10-04 22:11:37 +0700

    Update buildList.js

--- src/buildList.js
+++ src/buildList.js
@@ -21,7 +21,7 @@ module.exports = function buildList() {
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
-          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+          //return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
       }),
